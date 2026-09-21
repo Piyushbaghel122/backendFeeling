2cd7c7dc95e7b3304e288a6e085f565651094b18
Piyush Kumar <piyushkumar898923@gmail.com>
2026-09-21 17:04:38 +0530

    lopiwr

--- src/config/redis.go
+++ src/config/redis.go
@@ -10,26 +10,26 @@ import (
 
 var RedisClient *redis.Client
 
-const redis_url = "redis://default:H1OuWhD69wVLnfd1BfTTA2WU2xNMcOX2@redis-17276.c281.us-east-1-2.ec2.cloud.redislabs.com:17276"
-
 func ConnectRedis() {
 	ctx := context.Background()
 
-	// Get address from environment variable, fallback to 127.0.0.1:6379
-	redisAddr := os.Getenv("REDIS_ADDR")
-	if redisAddr == "" {
-		redisAddr = "127.0.0.1:6379" // Use 127.0.0.1 to avoid IPv6 issues on Windows
+	redisURL := os.Getenv("REDIS_URL")
+
+	if redisURL == "" {
+		log.Fatal("REDIS_URL is not set")
 	}
 
-	RedisClient = redis.NewClient(&redis.Options{
-		Addr: redisAddr,
-	})
-
-	// Ping the database to ensure connection is working
-	_, err := RedisClient.Ping(ctx).Result()
+	opt, err := redis.ParseURL(redisURL)
 	if err != nil {
-		log.Fatalf("Error connecting to Redis: %v\n", err)
+		log.Fatalf("Invalid Redis URL: %v", err)
 	}
-	
-	log.Println("Connected to Redis successfully!")
+
+	RedisClient = redis.NewClient(opt)
+
+	_, err = RedisClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Error connecting to Redis: %v", err)
+	}
+
+	log.Println("✅ Connected to Redis successfully!")
 }
